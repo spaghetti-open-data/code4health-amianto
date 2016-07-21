1af67837f9505d3e61e12697f5bbabf03e08c9b2
Davide Galletti <davidegalletti@users.noreply.github.com>
2016-07-21 23:29:46 +0200

    Fix errore mapquest
    MapQuest: direct tile access discontinued 
    Adesso uso Thunderforest www.thunderforest.com

--- segnalazionibot/static/maps/segnalazioni.js
+++ segnalazionibot/static/maps/segnalazioni.js
@@ -1,15 +1,19 @@
-// See post: http://asmaloney.com/2014/01/code/creating-an-interactive-map-with-leaflet-and-openstreetmap/
-
-var map = L.map( 'map', {
-    center: [41.0, 12.0],
-    minZoom: 3,
-    zoom: 5
+var map = new L.Map('map', {
+  center: new L.LatLng(41.7, 12.0),
+  minZoom: 3,
+  zoom: 6
 });
 
-L.tileLayer( 'http://{s}.mqcdn.com/tiles/1.0.0/map/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="http://osm.org/copyright" title="OpenStreetMap" target="_blank">OpenStreetMap</a> contributors | Tiles Courtesy of <a href="http://www.mapquest.com/" title="MapQuest" target="_blank">MapQuest</a> <img src="http://developer.mapquest.com/content/osm/mq_logo.png" width="16" height="16">',
-    subdomains: ['otile1','otile2','otile3','otile4']
-}).addTo( map );
+// create a new tile layer
+var tileUrl = 'https://{s}.tile.thunderforest.com/cycle/{z}/{x}/{y}.png',
+layer = new L.TileLayer(tileUrl, {
+    maxZoom: 18,
+    attribution: 'Data &copy; <a href="http://osm.org/copyright" title="OpenStreetMap" target="_blank">OpenStreetMap</a> contributors | Maps &copy; <a href="http://www.thunderforest.com/" title="Thunderforest" target="_blank">Thunderforest</a>',
+    });
+
+// add the layer to the map
+map.addLayer(layer);
+
 
 var myURL = jQuery( 'script[src$="segnalazioni.js"]' ).attr( 'src' ).replace( 'segnalazioni.js', '' );
 
@@ -27,3 +31,4 @@ for ( var i=0; i < markers.length; ++i )
       .bindPopup( '<a target="_blank" href="/static/' + markers[i].hires + '"/><img src="/static/' + markers[i].thumb + '"/></a><br>' + markers[i].when + ' ' + markers[i].caption )
       .addTo( map );
 }
+
